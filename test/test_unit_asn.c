6cf4afe77aaa850206c4feadea275a918864d9b7
Mattias.Konkov <mattias.konkov@guardtime.com>
2018-05-22 16:00:04 +0300

    ASN1 header parsing now accepts DER encoding only.

--- test/test_unit_asn.c
+++ test/test_unit_asn.c
@@ -303,6 +303,267 @@ static void Test_Asn1_Dom_get_subobject(CuTest* tc) {
 	asn1_dom_free(dom);
 }
 
+static void Test_Asn1_Dom_parse_header_correct_encoding(CuTest* tc) {
+	int res = LEGACY_UNKNOWN_ERROR;
+	asn1_object obj;
+	const unsigned char data[][20] = {
+		{0x01, 0x01, 0xff},
+		{0x02, 0x02, 0x00, 0x80},
+		{0x03, 0x04, 0x06, 0x6e, 0x5d, 0xe0},
+		{0x04, 0x06, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15},
+		{0x05, 0x00},
+		{0x06, 0x06, 0x2a, 0x86, 0x48, 0x86, 0xf7, 0x0d},
+		{0x13, 0x0c, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64, 0x21},
+		{0x14, 0x0d, 0x48, 0xc2, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64, 0x21},
+		{0x16, 0x0c, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64, 0x21},
+		{0x17, 0x0d, 0x39, 0x31, 0x30, 0x35, 0x30, 0x36, 0x32, 0x33, 0x34, 0x35, 0x34, 0x30, 0x5a},
+		{0x30, 0x07, 0x01, 0x01, 0xff, 0x02, 0x02, 0x00, 0x80},
+		{0x31, 0x08, 0x03, 0x04, 0x06, 0x6e, 0x5d, 0xe0, 0x05, 0x00},
+		{0x42, 0x02, 0x00, 0x80},
+		{0x82, 0x02, 0x00, 0x80},
+		{0xc2, 0x02, 0x00, 0x80},
+		{0x62, 0x02, 0x00, 0x80},
+		{0xa2, 0x02, 0x00, 0x80},
+		{0xe2, 0x02, 0x00, 0x80},
+		{0x70, 0x07, 0x01, 0x01, 0xff, 0x02, 0x02, 0x00, 0x80},
+		{0xb0, 0x07, 0x01, 0x01, 0xff, 0x02, 0x02, 0x00, 0x80},
+		{0xf0, 0x07, 0x01, 0x01, 0xff, 0x02, 0x02, 0x00, 0x80},
+		{0x50, 0x07, 0x01, 0x01, 0xff, 0x02, 0x02, 0x00, 0x80},
+		{0x90, 0x07, 0x01, 0x01, 0xff, 0x02, 0x02, 0x00, 0x80},
+		{0xd0, 0x07, 0x01, 0x01, 0xff, 0x02, 0x02, 0x00, 0x80},
+	};
+
+	res = asn1_parse_header(NULL, 2, &obj);
+	CuAssert(tc, "NULL buffer should not be allowed.", res == LEGACY_INVALID_ARGUMENT);
+
+	res = asn1_parse_header(&data[0][0], 1, &obj);
+	CuAssert(tc, "Length shorter than 2 should not be allowed.", res == LEGACY_INVALID_ARGUMENT);
+
+	res = asn1_parse_header(&data[0][0], 2, NULL);
+	CuAssert(tc, "NULL ASN1 object should not be allowed.", res == LEGACY_INVALID_ARGUMENT);
+
+	res = asn1_parse_header(&data[0][0], 3, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 1 && obj.header_length == 2 && obj.body_length == 1);
+
+	res = asn1_parse_header(&data[1][0], 4, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 2 && obj.header_length == 2 && obj.body_length == 2);
+
+	res = asn1_parse_header(&data[2][0], 6, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 3 && obj.header_length == 2 && obj.body_length == 4);
+
+	res = asn1_parse_header(&data[3][0], 8, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 4 && obj.header_length == 2 && obj.body_length == 6);
+
+	res = asn1_parse_header(&data[4][0], 2, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 5 && obj.header_length == 2 && obj.body_length == 0);
+
+	res = asn1_parse_header(&data[5][0], 8, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 6 && obj.header_length == 2 && obj.body_length == 6);
+
+	res = asn1_parse_header(&data[6][0], 14, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 19 && obj.header_length == 2 && obj.body_length == 12);
+
+	res = asn1_parse_header(&data[7][0], 15, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 20 && obj.header_length == 2 && obj.body_length == 13);
+
+	res = asn1_parse_header(&data[8][0], 14, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 22 && obj.header_length == 2 && obj.body_length == 12);
+
+	res = asn1_parse_header(&data[9][0], 15, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 23 && obj.header_length == 2 && obj.body_length == 13);
+
+	res = asn1_parse_header(&data[10][0], 9, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 16 && obj.header_length == 2 && obj.body_length == 7);
+
+	res = asn1_parse_header(&data[11][0], 10, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 17 && obj.header_length == 2 && obj.body_length == 8);
+
+	res = asn1_parse_header(&data[12][0], 4, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 2 && obj.header_length == 2 && obj.body_length == 2);
+
+	res = asn1_parse_header(&data[13][0], 4, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 2 && obj.header_length == 2 && obj.body_length == 2);
+
+	res = asn1_parse_header(&data[14][0], 4, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 2 && obj.header_length == 2 && obj.body_length == 2);
+
+	res = asn1_parse_header(&data[15][0], 4, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 2 && obj.header_length == 2 && obj.body_length == 2);
+
+	res = asn1_parse_header(&data[16][0], 4, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 2 && obj.header_length == 2 && obj.body_length == 2);
+
+	res = asn1_parse_header(&data[17][0], 4, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 2 && obj.header_length == 2 && obj.body_length == 2);
+
+	res = asn1_parse_header(&data[18][0], 9, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 16 && obj.header_length == 2 && obj.body_length == 7);
+
+	res = asn1_parse_header(&data[19][0], 9, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 16 && obj.header_length == 2 && obj.body_length == 7);
+
+	res = asn1_parse_header(&data[20][0], 9, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 16 && obj.header_length == 2 && obj.body_length == 7);
+
+	res = asn1_parse_header(&data[21][0], 9, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 16 && obj.header_length == 2 && obj.body_length == 7);
+
+	res = asn1_parse_header(&data[22][0], 9, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 16 && obj.header_length == 2 && obj.body_length == 7);
+
+	res = asn1_parse_header(&data[23][0], 9, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 16 && obj.header_length == 2 && obj.body_length == 7);
+
+
+}
+
+static void Test_Asn1_Dom_parse_header_incorrect_encoding(CuTest* tc) {
+	int res = LEGACY_UNKNOWN_ERROR;
+	asn1_object obj;
+	const unsigned char data[][20] = {
+		{0x21, 0x01, 0xff},
+		{0x22, 0x02, 0x00, 0x80},
+		{0x23, 0x04, 0x06, 0x6e, 0x5d, 0xe0},
+		{0x24, 0x06, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15},
+		{0x25, 0x00},
+		{0x26, 0x06, 0x2a, 0x86, 0x48, 0x86, 0xf7, 0x0d},
+		{0x33, 0x0c, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64, 0x21},
+		{0x34, 0x0d, 0x48, 0xc2, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64, 0x21},
+		{0x36, 0x0c, 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x20, 0x57, 0x6f, 0x72, 0x6c, 0x64, 0x21},
+		{0x37, 0x0d, 0x39, 0x31, 0x30, 0x35, 0x30, 0x36, 0x32, 0x33, 0x34, 0x35, 0x34, 0x30, 0x5a},
+		{0x10, 0x07, 0x01, 0x01, 0xff, 0x02, 0x02, 0x00, 0x80},
+		{0x11, 0x08, 0x03, 0x04, 0x06, 0x6e, 0x5d, 0xe0, 0x05, 0x00}
+	};
+
+	res = asn1_parse_header(&data[0][0], 3, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_INVALID_FORMAT);
+
+	res = asn1_parse_header(&data[1][0], 4, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_INVALID_FORMAT);
+
+	res = asn1_parse_header(&data[2][0], 6, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_INVALID_FORMAT);
+
+	res = asn1_parse_header(&data[3][0], 8, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_INVALID_FORMAT);
+
+	res = asn1_parse_header(&data[4][0], 2, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_INVALID_FORMAT);
+
+	res = asn1_parse_header(&data[5][0], 8, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_INVALID_FORMAT);
+
+	res = asn1_parse_header(&data[6][0], 14, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_INVALID_FORMAT);
+
+	res = asn1_parse_header(&data[7][0], 15, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_INVALID_FORMAT);
+
+	res = asn1_parse_header(&data[8][0], 14, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_INVALID_FORMAT);
+
+	res = asn1_parse_header(&data[9][0], 15, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_INVALID_FORMAT);
+
+	res = asn1_parse_header(&data[10][0], 9, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_INVALID_FORMAT);
+
+	res = asn1_parse_header(&data[11][0], 10, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_INVALID_FORMAT);
+}
+
+static void Test_Asn1_Dom_parse_header_tag_and_length(CuTest* tc) {
+	int res = LEGACY_UNKNOWN_ERROR;
+	asn1_object obj;
+	const unsigned char data[][20] = {
+		{0x01, 0x80, 0xff, 0x00, 0x00},
+		{0x02, 0x81, 0x7f, 0x00, 0x80},
+		{0x02, 0x81, 0x80, 0x00, 0x80},
+		{0x03, 0x83, 0x00, 0x01, 0x80, 0x6e, 0x5d, 0xe0},
+		{0x03, 0x82, 0x01, 0x80, 0x6e, 0x5d, 0xe0},
+		{0x1f, 0x1e, 0x06, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15},
+		{0x1e, 0x06, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15},
+		{0x1f, 0x80, 0x1f, 0x06, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15},
+		{0x1f, 0x1f, 0x06, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15},
+		{0x1f, 0x81, 0x00, 0x06, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15}
+	};
+
+	res = asn1_parse_header(&data[0][0], 3, &obj);
+	CuAssert(tc, "Indefinite length not allowed.", res == LEGACY_INVALID_FORMAT);
+
+	res = asn1_parse_header(&data[1][0], 130, &obj);
+	CuAssert(tc, "Length below 128 must be encoded in short form.", res == LEGACY_INVALID_FORMAT);
+
+	res = asn1_parse_header(&data[2][0], 131, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 2 && obj.header_length == 3 && obj.body_length == 128);
+
+	res = asn1_parse_header(&data[2][0], 130, &obj);
+	CuAssert(tc, "The buffer size must be large enough for ASN1 object.", res == LEGACY_ASN1_PARSING_ERROR);
+
+	res = asn1_parse_header(&data[3][0], 389, &obj);
+	CuAssert(tc, "Length must be encoded with the minimum number of octets.", res == LEGACY_INVALID_FORMAT);
+
+	res = asn1_parse_header(&data[4][0], 388, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 3 && obj.header_length == 4 && obj.body_length == 384);
+
+	res = asn1_parse_header(&data[4][0], 387, &obj);
+	CuAssert(tc, "The buffer size must be large enough for ASN1 object.", res == LEGACY_ASN1_PARSING_ERROR);
+
+	res = asn1_parse_header(&data[5][0], 9, &obj);
+	CuAssert(tc, "High-tag-form is reserved for tag numbers 31 and higher.", res == LEGACY_INVALID_FORMAT);
+
+	res = asn1_parse_header(&data[6][0], 8, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 30 && obj.header_length == 2 && obj.body_length == 6);
+
+	res = asn1_parse_header(&data[6][0], 7, &obj);
+	CuAssert(tc, "The buffer size must be large enough for ASN1 object.", res == LEGACY_ASN1_PARSING_ERROR);
+
+	res = asn1_parse_header(&data[7][0], 10, &obj);
+	CuAssert(tc, "Tag number must be encoded with the minimum number of octers.", res == LEGACY_INVALID_FORMAT);
+
+	res = asn1_parse_header(&data[8][0], 9, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 31 && obj.header_length == 3 && obj.body_length == 6);
+
+	res = asn1_parse_header(&data[8][0], 8, &obj);
+	CuAssert(tc, "The buffer size must be large enough for ASN1 object.", res == LEGACY_ASN1_PARSING_ERROR);
+
+	res = asn1_parse_header(&data[9][0], 10, &obj);
+	CuAssert(tc, "Unable to parse ASN object header.", res == LEGACY_OK);
+	CuAssert(tc, "ASN1 object header parsed incorrectly.", obj.tag == 128 && obj.header_length == 4 && obj.body_length == 6);
+
+	res = asn1_parse_header(&data[9][0], 9, &obj);
+	CuAssert(tc, "The buffer size must be large enough for ASN1 object.", res == LEGACY_ASN1_PARSING_ERROR);
+
+}
+
 CuSuite* LegacyTest_ASN_getSuite(void)
 {
 	CuSuite* suite = CuSuiteNew();
@@ -312,6 +573,9 @@ CuSuite* LegacyTest_ASN_getSuite(void)
 	SUITE_ADD_TEST(suite, Test_Asn1_Dom_add_object);
 	SUITE_ADD_TEST(suite, Test_Asn1_Dom_get_child);
 	SUITE_ADD_TEST(suite, Test_Asn1_Dom_get_subobject);
+	SUITE_ADD_TEST(suite, Test_Asn1_Dom_parse_header_correct_encoding);
+	SUITE_ADD_TEST(suite, Test_Asn1_Dom_parse_header_incorrect_encoding);
+	SUITE_ADD_TEST(suite, Test_Asn1_Dom_parse_header_tag_and_length);
 
 	return suite;
 }
